c88c4ee4b4db90ac997e762e2ffd2137d5893213
Marijn van Aerle <73708+mvaerle@users.noreply.github.com>
2025-02-24 12:58:35 +0100

    basic splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ShaderGradientCanvas
+        style={{
+          width: '100vw',
+          height: '100vh',
+          position: 'relative'
+        }}
+      >
+        <ShaderGradient
+          control='query'
+          urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=1.1&cAzimuthAngle=180&cDistance=3.9&cPolarAngle=115&cameraZoom=1&color1=%2300008e&color2=%23005800&color3=%23000000&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=45&frameRate=10&grain=off&lightType=env&pixelDensity=1&positionX=-0.5&positionY=0.1&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0.2&rotationX=0&rotationY=0&rotationZ=235&shader=defaults&toggleAxis=false&type=plane&uAmplitude=0&uDensity=1.1&uFrequency=5.5&uSpeed=0.2&uStrength=2.4&uTime=0.2&wireframe=false&zoomOut=false'
+        />
+      </ShaderGradientCanvas>
+      <div style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        color: 'white',
+        fontSize: '4rem',
+        fontWeight: 'bold',
+      }}>
+        Konstruct
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div style={{
+        position: 'absolute',
+        bottom: '2rem',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        color: 'white',
+        textAlign: 'center',
+        lineHeight: '1.5',
+      }}>
+        <div style={{ fontSize: '1.0rem', fontWeight: 'bold' }}>
+          Konstruct B.V.
+        </div>
+        <div>Karel Ensinckstraat 40B</div>
+        <div>5246 GM 's-Hertogenbosch</div>
+        <div>+31(0)6 464 22 181</div>
+        <div><a href="mailto:marijn@konstruct.nl" style={{ color: 'white', textDecoration: 'none' }}>marijn@konstruct.nl</a></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
